392b866ad0d2fc6ab5a2453aeee679c491e12401
Stephen Leigh <leigh.s95@outlook.com>
2019-05-26 12:12:11 +0100

    added user components to UserFormDetails

--- src/components/FormUserDetails.js
+++ src/components/FormUserDetails.js
@@ -1,12 +1,54 @@
 import React, { Component } from 'react'
+import MuiTheme from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton'
 
 export class FormUserDetails extends Component {
+    continue = e => {
+        e.preventDefault();
+        this.props.nextStep();
+    }
+
     render() {
+        const { values, handleChange } = this.props;
         return (
-            <div>
-                <h1>hello</h1>
-            </div>
+            <MuiTheme>
+                <React.Fragment>
+                    <AppBar title="Enter Details" />
+                    <TextField 
+                        hintText = "Enter First Name"
+                        floatingLabelText = "First Name"
+                        onChange = { handleChange('firstName') }
+                        defaultValue = { values.firstName }
+                    />
+                    <TextField 
+                        hintText = "Enter Last Name"
+                        floatingLabelText = "Last Name"
+                        onChange = { handleChange('lastName') }
+                        defaultValue = { values.lastName }
+                    />
+                    <TextField 
+                        hintText = "Enter Email"
+                        floatingLabelText = "Email"
+                        onChange = { handleChange('email') }
+                        defaultValue = { values.email }
+                    />
+                    <RaisedButton 
+                        label = "Continue"
+                        primary = { true }
+                        style = { styles.button }
+                        onClick = { this.continue }
+                    />
+                </React.Fragment>
+            </MuiTheme>
         )
+    }
+}
+
+const styles = {
+    button: {
+        margin: 15
     }
 }
 
